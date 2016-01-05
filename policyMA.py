929c9d71699e044edfc583b6dd813c6227906175
wkf2000 <wkf2000@hotmail.com>
2016-01-04 22:35:09 -0800

    minor

--- policyMA.py
+++ policyMA.py
@@ -1,71 +1,86 @@
 #!/usr/bin/env python
 
-import sp500data
 import os
 import datetime
 import pandas as pd
+import csv
 
-RANGE = range(-1, 2)
+RANGE = range(-10, 11)
 RANGE.insert(0, -1000)
 RANGE.append(1000)
 PERIOD = 20
 GAIN = 10
+PATH = 'data/'
+OUTPATH = 'out/'
 
 
 def init_dict():
-    dict = {}
+    ret = {}
     for i in range(len(RANGE)-1):
         for j in range(len(RANGE)-1):
             key = "[%d,%d][%d,%d]" % (RANGE[i], RANGE[i+1], RANGE[j], RANGE[j+1])
-            dict[key] = 0
-    return dict
+            ret[key] = 0
+    return ret
 
 
 def find_range(ma10, ma100):
     for i in range(len(RANGE)-1):
         for j in range(len(RANGE)-1):
-            if RANGE[i] < ma10 and ma10 > RANGE[i+1] and RANGE[j] < ma100 and ma100 > RANGE[j+1]:
+            if RANGE[i] < ma10 < RANGE[i+1] and RANGE[j] < ma100 < RANGE[j+1]:
                 return "[%d,%d][%d,%d]" % (RANGE[i], RANGE[i+1], RANGE[j], RANGE[j+1])
     return "error"
 
 
-def zhang_10pct():
-    return True
+def zhang_10pct(df, date):
+    pp = df.loc[date:date + PERIOD*datetime.timedelta(1), 'Close']
+    chg = (pp.max() - pp[0]) / pp[0]
+    if chg > 0.1:
+        return True
+    return False
+
+
+def update_result(df, date, result):
+    ma10 = df.loc[date, '10_MAC']
+    ma100 = df.loc[date, '100_MAC']
+    key = find_range(ma10, ma100)
+    if key == "error":
+        return
+    result[key] += 1
 
 
 def calculation(symbol):
     in_file = os.path.join('data', symbol + '.csv')
     df = pd.read_csv(in_file, index_col='Date', parse_dates=True, usecols=['Date', 'Close', '10_MAC', '100_MAC'])
-    # print df.info()
 
-    dict = init_dict()
+    result = init_dict()
 
     for date in df.index:
-        print date
-    # print dict
-    # dates = pd.bdate_range(start=sp500data.START, end=sp500data.END, closed='left')
-    # print dates
-    # for date in dates:
-    #     print df.loc[date]
+        if zhang_10pct(df, date):
+            update_result(df, date, result)
 
-    # rng = pd.bdate_range(start=sp500data.START, periods=PERIOD)
-    # df1 = df.loc[rng, 'Close']
-    # print df1[0]
-    # print df1.max()
+    # writer = csv.writer(open(PATH + symbol + '.out.csv', 'w'))
+    # with open(OUTPATH + symbol + '.txt', 'w') as f:
+    #     writer = csv.DictWriter(f, result.keys())
+    #     writer.writerow(result)
 
-    # for index, row in df.iterrows():
-    #     print row
-
-    # for row in df:
-    #     if(>10% in 20 days):
-    #         update_dict(ma10, ma100 ,)
-
+    headings = ['key', 'value']
+    try:
+        with open(OUTPATH + symbol + '.txt','w') as myCSVFile:
+            csvWriter = csv.DictWriter(myCSVFile, fieldnames=headings, dialect='excel')
+            csvWriter.writeheader()
+            for data in result:
+                csvWriter.writerow(data)
+    except IOError as (errno, strerror):
+        print("I/O error({0}): {1}".format(errno, strerror))
+    return
 
 if __name__ == '__main__':
-    path = 'data/'
-    for file in os.listdir(path):
-        symbol = os.path.splitext(file)[0]
-        print "working on " + symbol
-        calculation(symbol)
-        break
+    if not os.path.exists('out'):
+        os.makedirs('out')
+
+    for files in os.listdir(PATH):
+        symbol, ext = os.path.splitext(files)
+        if ext == '.csv':
+            print "working on " + symbol
+            calculation(symbol)
 
